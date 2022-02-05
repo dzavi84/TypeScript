8f5ce39dfb12ef4b067aff8ad6d5736088023ea8
dzavi84 <milanvidanovic@yahoo.com>
2022-02-05 17:08:15 +0100

    commit2

--- app.js
+++ app.js
@@ -2,8 +2,27 @@
 //   name: string;
 //   age: number;
 // } =
+// const person = {
+//   name: 'Milan',
+//   age: 38,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author'],
+// };
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: 'Milan',
-    age: 38
+    age: 38,
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN
 };
+var favoriteActivities;
+favoriteActivities = ['Sports'];
 console.log(person.name);
+if (person.role === Role.AUTHOR) {
+    console.log('is author');
+}
--- app.ts
+++ app.ts
@@ -2,9 +2,29 @@
 //   name: string;
 //   age: number;
 // } =
+// const person = {
+//   name: 'Milan',
+//   age: 38,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author'],
+// };
+
+enum Role {
+  ADMIN = 'ADMIN',
+  READ_ONLY = 100,
+  AUTHOR,
+}
+
 const person = {
   name: 'Milan',
   age: 38,
+  hobbies: ['Sports', 'Cooking'],
+  role: Role.ADMIN,
 };
-
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
 console.log(person.name);
+
+if (person.role === Role.AUTHOR) {
+  console.log('is author');
+}
