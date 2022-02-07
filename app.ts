ff6aa3a94e6395eecd83c1c4f0a775b766d1c142
dzavi84 <milanvidanovic@yahoo.com>
2022-02-07 13:01:07 +0100

    commit1

--- app.ts
+++ app.ts
@@ -1,31 +1,9 @@
-function combine(
-  input1: number | string,
-  input2: number | string,
-  resultConversion: string
-) {
-  let result;
-  if (
-    (typeof input1 === 'number' && typeof input2 === 'number') ||
-    resultConversion === 'as-number'
-  ) {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  return result;
-  //   if (resultConversion === 'as-number') {
-  //     return +result;
-  //   } else {
-  //     return result.toString();
-  //   }
+function add(n1: number, n2: number) {
+  return n1 + n2;
 }
 
-const combineAges = combine(30, 26, 'as-number');
+function printResult(num: number) {
+  console.log('Result:' + num);
+}
 
-console.log(combineAges);
-
-const combineStringAges = combine('30', '26', 'as-number');
-console.log(combineStringAges);
-
-const combineNames = combine('Max', 'Anna', 'as-text');
-console.log(combineNames);
+printResult(add(5, 12));
