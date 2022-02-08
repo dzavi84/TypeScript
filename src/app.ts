478e6e6b07fe8d1dc502b55a802d7412477fa69e
dzavi84 <milanvidanovic@yahoo.com>
2022-02-08 13:04:18 +0100

    commit1

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
-const button = document.querySelector('button')!;
+const button = document.querySelector('button');
 
-button.addEventListener('click', () => {
-  console.log('Clicked!');
-});
+if (button) {
+  button.addEventListener('click', () => {
+    console.log('Clicked!');
+  });
+}
