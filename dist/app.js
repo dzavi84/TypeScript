f1371cbfed43d1523f1fb5f5e970a8ace598f9c5
dzavi84 <milanvidanovic@yahoo.com>
2022-02-09 12:54:58 +0100

    commit1

--- dist/app.js
+++ dist/app.js
@@ -1,14 +1,25 @@
 "use strict";
 class Department {
     constructor(n) {
+        this.employees = [];
         this.name = n;
     }
     describe() {
         console.log('Department:' + this.name);
     }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 const accounting = new Department('Accounting');
+accounting.addEmployee('Max');
+accounting.addEmployee('Milan');
 console.log(accounting);
 accounting.describe();
-const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
-accountingCopy.describe();
+accounting.printEmployeeInformation();
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+// accountingCopy.describe();
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 class Department {
   name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
@@ -7,12 +8,25 @@ class Department {
   describe(this: Department) {
     console.log('Department:' + this.name);
   }
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department('Accounting');
+
+accounting.addEmployee('Max');
+accounting.addEmployee('Milan');
+
 console.log(accounting);
 
 accounting.describe();
+accounting.printEmployeeInformation();
 
-const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
-accountingCopy.describe();
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+// accountingCopy.describe();
