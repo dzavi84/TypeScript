ffe639e681a637be306a8e1616c3a47a8dde0b81
dzavi84 <milanvidanovic@yahoo.com>
2022-02-15 16:24:34 +0100

    commit

--- dist/app.js
+++ dist/app.js
@@ -16,3 +16,14 @@ function merge(objA, objB) {
 const mergedObj = merge({ name: 'Milan', hobbies: ['sport'] }, { age: 37 });
 const mergedObj2 = merge({ name: 'Milan' }, { age: 37 });
 console.log(mergedObj.age);
+function countAndDescribe(element) {
+    let descriptionText = 'Got no value';
+    if (element.length === 1) {
+        descriptionText = 'Got 1 element.';
+    }
+    else if (element.length > 0) {
+        descriptionText = 'Got ' + element.length + ' elements.';
+    }
+    return [element, descriptionText];
+}
+console.log(countAndDescribe('Hi there!'));
