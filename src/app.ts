7c09cea744e0745359a4589076362c98670199de
dzavi84 <milanvidanovic@yahoo.com>
2022-02-16 20:47:36 +0100

    commit3

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,27 @@
 function Logger(logString: string) {
+  console.log('LOGGER FACTORY');
   return function (constructor: Function) {
     console.log(logString);
     console.log(constructor);
   };
 }
-@Logger('LOGGING-PERSON')
+
+function WithTemplate(template: string, hookId: string) {
+  console.log('TEMPLATE FACTORY');
+  return function (constructor: any) {
+    console.log('Rendering template');
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector('h1')!.textContent = p.name;
+    }
+  };
+}
+
+// @Logger('LOGGING-PERSON')
+@Logger('Logging!!')
+@WithTemplate('<h1>My Person Object</h1>', 'app')
 class Person {
   name = 'Milan';
 
